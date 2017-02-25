95d96225763be7bfd0cca7d377f5060bca763dea
EliL123 <20206028@nvnet.org>
2017-02-25 10:47:23 -0500

    Update Enemy AI.cs

--- Assets/Scripts/Enemy AI.cs
+++ Assets/Scripts/Enemy AI.cs
@@ -1,3 +1,4 @@
+
 using UnityEngine;
 using System.Collections;
 
@@ -8,65 +9,38 @@ public class EnemyAI : MonoBehaviour
 	public float maxSpeed = 8f;
 	public float gravity = 5f;
 	public GameObject Enemy; 
+	public Rigidbody2D rigidEnemy;
 	public GameObject Playera; 
-	public bool destroy;
 
 	Rect box;
 
 	Vector2 velocity; 
 
-	bool isVisible = true;
-
 	Vector2 walkAmount;
 
 	void Start()
-    {
+	{
 		GameObject Enemy = GameObject.FindGameObjectWithTag("Enemy").GetComponent<GameObject>();
 		GameObject Playera = GameObject.FindGameObjectWithTag("Playera").GetComponent<GameObject>();
 	}
 
 	void FixedUpdate()
 	{ 
-		if(destroy == true)
-        {
-			Playera.SetActive(false);
-		}
 
-		if (destroy == false)
-        {
-			Playera.SetActive(true);
-		}
-
-		walkAmount.x = acceleration * maxSpeed * Time.deltaTime;
-		transform.Translate(walkAmount);
+		rigidEnemy = Enemy.GetComponent<Rigidbody2D>(); 
+		rigidEnemy.velocity = (Vector2.left * 30);
+		rigidEnemy.freezeRotation = true;
 
 		float newVelocityX = velocity.x;
-
-		if (isVisible == true)
-        { 
-			newVelocityX += acceleration;
-			newVelocityX = Mathf.Clamp (newVelocityX, -maxSpeed, maxSpeed);
-		}
-
-		velocity = new Vector2(newVelocityX, velocity.y);
 
 	}
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
-		if(coll.gameObject.name == "Playera")
-        {
-			destroy = true;
-		}
-        else
-        {
-			destroy = false;
+		if (coll.gameObject.name == "Playera") {
+
+			Destroy (coll.gameObject);
+
 		}
 	}
-
-	void LateUpdate()
-	{
-		transform.Translate (velocity * Time.deltaTime);
-	}
-
 }
