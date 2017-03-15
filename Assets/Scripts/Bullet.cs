66815c5a8276eb9b5e719b74f66fe956d6f97c4a
DrEvilBrain <drevilbrain@gmail.com>
2017-03-14 22:39:48 -0400

    Temporary Shooting
    I'm tired and I'll fix this up later

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Debug.Log("Bullet Hit Enemy"); //Off screen bullet killing zones are counted as enemies for ease of purpose
+            Destroy(gameObject);
+        }
+
+    }
+
+}
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -6,19 +6,17 @@ public class Shooting : MonoBehaviour
 	//Make child gameobject of Player and attach this script to it
 
 	public GameObject bullet; 
-	public GameObject bulletClone;
-	public Rigidbody2D bulletPrefab;
+	private GameObject bulletClone;
+    private Rigidbody2D bulletPrefab;
 
-	void FixedUpdate ()
+    void FixedUpdate()
 	{
-
-		if(Input.GetButtonDown("Shoot"))
+		if(Input.GetButtonDown("Fire1"))
 		{
 			bulletClone = Instantiate (bullet, transform.position, Quaternion.identity) as GameObject; //Instantiate the bullet as a GameObject
 			bulletPrefab = bulletClone.GetComponent<Rigidbody2D>(); 
-			bulletPrefab.velocity = (Vector2.right * 250); //Take the rigidbody of the bullet clone and add force to it
-			Destroy(bulletClone, 2); //Destroys the bullets after 2 seconds
+			bulletPrefab.velocity = (Vector2.right * 10); //Take the rigidbody of the bullet clone and add force to it
 		}
+	}
 
-	}		
 }
