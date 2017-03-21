a7d95ce149e563615b07f11f255cf648aeac1f04
EliL123 <20206028@nvnet.org>
2017-03-20 20:25:26 -0400

    Add files via upload

---
+++ OptionButton.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+//A Canvas and Button needs to be made. Add to the On Click () function and attach this script. 
+//Go to build settings and add the scene that will begin the game. 
+//When you click the Start button, the scene put in the scenes menu will load.
+
+public class OptionButton : MonoBehaviour {
+
+		public void LoadByIndex(int sceneIndex)
+		{
+			SceneManager.LoadScene (sceneIndex);
+	}
+}
